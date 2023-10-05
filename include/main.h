6ee1d22d10959141830ddc0cf68dcf0200b64f1c
LinuxlasGNUleaf <jakob.l.weiss@gmx.de>
2023-10-05 16:52:37 +0200

    feature: speed display in fps and kmh

--- include/main.h
+++ include/main.h
@@ -5,4 +5,4 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 const double fps_to_kmh = 1.09728f;
 
-void drawFPS(int fps);
+void drawSpeed(float fps);
--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,34 @@ void setup()
   u8g2.setFontDirection(0);
 }
 
+int i = 0;
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  for (int i = 0; i <= 99; i++)
-  {
-    u8g2.clearBuffer();
-    drawFPS(i);
-    u8g2.sendBuffer();
-    delay(150);
-  }
+  u8g2.clearBuffer();
+  drawSpeed(i++ % 1000);
+  u8g2.drawLine(0, 33+6, 127, 33+6);
+  drawStats()
+  u8g2.sendBuffer();
+  delay(150);
 }
 
 void drawSpeed(float fps)
 {
   char *buf_fps;
   char *buf_kmh;
-  buf_fps = new char[3];
+  buf_fps = new char[4];
   buf_kmh = new char[4];
-  sprintf(buf_fps, "%02d", constrain(round(fps),0,99));
-  sprintf(buf_kmh, "%03d", constrain(round(fps*fps_to_kmh),0,999));
+  sprintf(buf_fps, "%03.0f", constrain(fps, 0, 999));
+  sprintf(buf_kmh, "%03.0f", constrain(fps * fps_to_kmh, 0, 999));
 
-  u8g2.setFont(u8g2_font_inr42_mf);
-  u8g2.drawStr(2, 2, buf_fps);
-  u8g2.setFont(u8g2_font_inr21_mf);
-  u8g2.drawStr(36 * 2 + 2, 42-21, "fps");
+  // big fps display
+  u8g2.setFont(u8g2_font_inr33_mf);
+  u8g2.drawStr(0, 0, buf_fps);
+  u8g2.setFont(u8g2_font_inr16_mf);
+  u8g2.drawStr(28 * 3 + 2, 33 - 16, "fps");
+
+  // small km/h display
+  u8g2.setFont(u8g2_font_5x8_mf);
+  u8g2.drawStr(28 * 3 + 4, 2, buf_kmh);
+  u8g2.drawStr(28 * 3 + 4 + 5 * 4, 2, "km/h");
 }
