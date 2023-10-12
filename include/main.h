af167f51b4314b5e6b9a6757ace64d1fab3a4aee
TheLegolas <jakob.l.weiss@gmx.de>
2023-10-12 16:16:08 +0200

    feature: fps measurement and display

--- include/main.h
+++ include/main.h
@@ -7,19 +7,56 @@
 #include <dart_config.h>
 
 #define FPS_TO_MPS 0.3047999902464f
+#define MPS_TO_FPS 3.28084f
+
 #define CALC_MJOULE(fps, mass) (500*fps*fps*FPS_TO_MPS*FPS_TO_MPS*mass) // 0.5*mv^2
 
+// HARDWARE CONFIG
+const int buttonPin = D7;
+const int sensorPin1 = D5;
+const int sensorPin2 = D6;
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+const double sensor_distance = 5e-2;
 
+// SOFTWARE CONFIG
+#define ALL_SENSORS_FIRED 0b10001111
+#define ARMED_BIT         7
+#define ARMED             0b10000000
+#define IDLE              0b00000000
 
-void drawTopDisplay();
-void drawStats();
+#define MIN_US_DELAY 360
+#define DISPLAY_BLINK_INTERVAL 500
+
+#define GET_BIT(data, n) (1 & (data >> n))
+#define SET_BIT(data, n, value) (value ? data |= (1 << n) : data &= ~(1 << n))
+#define IS_ARMED() GET_BIT(state, ARMED_BIT)
+
+// methods
+void drawDisplayBuffer();
 void updateProperties();
+void finalize();
+void arm();
 
-unsigned long spt1, spt2, dpt1, dpt2;
+// interrupt routines
+void IRAM_ATTR SENSOR1_ISR();
+void IRAM_ATTR SENSOR2_ISR();
+
+// measurement variables
+double spt1, spt2, dpt1, dpt2;
+volatile unsigned long ts1_entry, ts1_exit, ts2_entry, ts2_exit;
+volatile byte state;
+volatile bool sensor1_level, sensor2_level, old_sensor1_level, old_sensor2_level;
+
+// result variables
+double vs1, vs2, vd1, vd2;
 double fps;
 double mass;
 double length;
 DART_TYPE type;
 
+// misc variables
+bool update_display;
+bool blink_state;
+unsigned long last_display_blink;
+unsigned long current_ms;
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,198 @@
 
 void setup()
 {
-  type = DART_NORMAL;
+  // configure sensors
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(sensorPin1, INPUT);
+  pinMode(sensorPin2, INPUT);
+
+  // reset variables
+  state = IDLE;
+  update_display = false;
+  type = DART_MEGA;
   updateProperties();
-  // put your setup code here, to run once:
+
+  // start display
   u8g2.begin();
   u8g2.setDrawColor(1);
   u8g2.setFontPosTop();
   u8g2.setFontDirection(0);
+
+  drawDisplayBuffer();
+  u8g2.sendBuffer();
+
+  // attach ISRs
+  attachInterrupt(sensorPin1, SENSOR1_ISR, CHANGE);
+  attachInterrupt(sensorPin2, SENSOR2_ISR, CHANGE);
+
+  Serial.begin(115200);
+
+  arm();
 }
 
-int i = 0;
+void printState()
+{
+  Serial.print("0b");
+  for (int i = 7; i >= 0; i--)
+  {
+    Serial.print(GET_BIT(state, i));
+  }
+  Serial.println();
+}
+
+byte old_state = IDLE;
 void loop()
 {
-  fps = i++ % 1000;
-  u8g2.clearBuffer();
-  drawTopDisplay();
-  drawStats();
-  u8g2.sendBuffer();
-  delay(150);
+  if (state != old_state)
+  {
+    printState();
+    old_state = state;
+  }
+  // if measurement is complete, start computation
+  if (state == ALL_SENSORS_FIRED)
+    finalize();
+
+  // if measurement has started, return
+  if (GET_BIT(state, 0) == 1)
+    return;
+
+  current_ms = millis();
+
+  if (current_ms - last_display_blink > DISPLAY_BLINK_INTERVAL && IS_ARMED())
+  {
+    blink_state = !blink_state;
+    last_display_blink = current_ms;
+    update_display = true;
+  }
+
+  // if not armed and update has been requested, redraw screen
+  if (update_display)
+  {
+    drawDisplayBuffer();
+    // if measurement hasn't started, continue
+    if (GET_BIT(state, 0) == 0)
+    {
+      u8g2.sendBuffer();
+      update_display = false;
+    }
+  }
 }
 
-void drawStats()
+void IRAM_ATTR SENSOR1_ISR()
 {
+  sensor1_level = digitalRead(sensorPin1);
+  // return when not armed
+  if (!IS_ARMED())
+  {
+    old_sensor1_level = sensor1_level;
+    return;
+  }
+
+  // entry point trigger when:
+  if (
+      // - sensor has NOT fired yet
+      GET_BIT(state, 0) == false &&
+      // - level is HIGH
+      sensor1_level == HIGH &&
+      // - old_level is low
+      old_sensor1_level == LOW)
+  {
+    ts1_entry = micros();
+    SET_BIT(state, 0, true);
+  }
+  // exit point trigger when:
+  else if (
+      // - entry point trigger has fired
+      GET_BIT(state, 0) == true &&
+      // - exit point trigger has NOT fired yet
+      GET_BIT(state, 1) == false &&
+      // - level is LOW
+      sensor1_level == LOW &&
+      // - old level is HIGH
+      old_sensor1_level == HIGH &&
+      // - MIN_DELAY has passed
+      (micros() - ts1_entry) > MIN_US_DELAY)
+  {
+    ts1_exit = micros();
+    SET_BIT(state, 1, true);
+  }
+
+  old_sensor1_level = sensor1_level;
+}
+
+void IRAM_ATTR SENSOR2_ISR()
+{
+  sensor2_level = digitalRead(sensorPin2);
+  // return when not armed OR sensor 1 has not fired yet
+  if (!IS_ARMED() || !GET_BIT(state, 0))
+  {
+    old_sensor2_level = sensor2_level;
+    return;
+  }
+
+  // entry point trigger when:
+  if (
+      // - sensor has NOT fired yet
+      GET_BIT(state, 2) == false &&
+      // - level is HIGH
+      sensor2_level == HIGH &&
+      // - old_level is low
+      old_sensor2_level == LOW)
+  {
+    ts2_entry = micros();
+    SET_BIT(state, 2, true);
+  }
+  // exit point trigger when:
+  else if (
+      // - entry point trigger has fired
+      GET_BIT(state, 2) == true &&
+      // - exit point trigger has NOT fired yet
+      GET_BIT(state, 3) == false &&
+      // - level is LOW
+      sensor2_level == LOW &&
+      // - old level is HIGH
+      old_sensor2_level == HIGH &&
+      // - MIN_DELAY has passed
+      (micros() - ts2_entry) > MIN_US_DELAY)
+  {
+    ts2_exit = micros();
+    SET_BIT(state, 3, true);
+  }
+
+  old_sensor2_level = sensor2_level;
+}
+
+void drawDisplayBuffer()
+{
+  u8g2.clearBuffer();
+  //==========> UPPER HALF OF DISPLAY
+  char *buf_fps;
+  char *buf_j;
+  buf_fps = new char[4];
+  buf_j = new char[5];
+  sprintf(buf_fps, "%03.0f", constrain(fps, 0, 999));
+  sprintf(buf_j, "%4.0f", constrain(CALC_MJOULE(fps, mass), 0.0f, 9999.0f));
+
+  // big fps display
+  // only draw FPS count when not armed or blink_state is true
+  if (!IS_ARMED() || blink_state)
+  {
+    u8g2.setFont(u8g2_font_inr33_mf);
+    u8g2.drawStr(0, 0, buf_fps);
+  }
+  u8g2.setFont(u8g2_font_inr16_mf);
+  u8g2.drawStr(28 * 3, 33 - 16, "fps");
+
+  // small energy display
+  u8g2.setFont(u8g2_font_5x8_mf);
+  u8g2.drawStr(28 * 3 + 4, 2, buf_j);
+  u8g2.drawStr(28 * 3 + 4 + 5 * 5, 2, "mJ");
+
+  // seperators
+  u8g2.drawLine(0, 33 + 6, 127, 33 + 6);
+  u8g2.drawLine(0, 33 + 8, 127, 33 + 8);
+
+  //==========> LOWER HALF OF DISPLAY
   // draw dart type
   u8g2.setFont(u8g2_font_12x6LED_tf);
   switch (type)
@@ -44,39 +214,17 @@ void drawStats()
   u8g2.setFont(u8g2_font_5x8_mf);
   u8g2.drawStr(1, 42, "projectile:");
 
-  // draw timings
+  // draw measurements
   u8g2.drawLine(56, 33 + 9, 56, 63);
   u8g2.setFont(u8g2_font_4x6_mf);
-  u8g2.drawStr(58, 42, "TIMINGS (ms):");
-  u8g2.drawStr(58, 42 + 8, "spt: xx | xx");
-  u8g2.drawStr(58, 42 + 16, "dpt: xx | xx");
+  u8g2.drawStr(58, 42, "RAW (fps):");
+  char buf[20];
+  sprintf(buf, "spt:%03.0f|%03.0f", min(vs1, 999.0), min(vs2, 999.0));
+  u8g2.drawStr(58, 42 + 8, buf);
+  sprintf(buf, "dpt:%03.0f|%03.0f", min(vd1, 999.0), min(vd2, 999.0));
+  u8g2.drawStr(58, 42 + 16, buf);
 
   // draw state
-}
-
-void drawTopDisplay()
-{
-  char *buf_fps;
-  char *buf_j;
-  buf_fps = new char[4];
-  buf_j = new char[5];
-  sprintf(buf_fps, "%03.0f", constrain(fps, 0, 999));
-  sprintf(buf_j, "%4.0f", constrain(CALC_MJOULE(fps, mass), 0.0f, 9999.0f));
-
-  // big fps display
-  u8g2.setFont(u8g2_font_inr33_mf);
-  u8g2.drawStr(0, 0, buf_fps);
-  u8g2.setFont(u8g2_font_inr16_mf);
-  u8g2.drawStr(28 * 3, 33 - 16, "fps");
-
-  // small energy display
-  u8g2.setFont(u8g2_font_5x8_mf);
-  u8g2.drawStr(28 * 3 + 4, 2, buf_j);
-  u8g2.drawStr(28 * 3 + 4 + 5 * 5, 2, "mJ");
-
-  // seperators
-  u8g2.drawLine(0, 33 + 6, 127, 33 + 6);
-  u8g2.drawLine(0, 33 + 8, 127, 33 + 8);
 }
 
 void updateProperties()
@@ -96,4 +244,33 @@ void updateProperties()
     length = length_dart_mega;
     break;
   }
+}
+
+void finalize()
+{
+  spt1 = (ts1_exit - ts1_entry) * 1e-6;
+  spt2 = (ts2_exit - ts2_entry) * 1e-6;
+  dpt1 = (ts2_entry - ts1_entry) * 1e-6;
+  dpt2 = (ts2_exit - ts1_exit) * 1e-6;
+
+  vs1 = (length / spt1) * MPS_TO_FPS;
+  vs2 = (length / spt2) * MPS_TO_FPS;
+  vd1 = (sensor_distance / dpt1) * MPS_TO_FPS;
+  vd2 = (sensor_distance / dpt2) * MPS_TO_FPS;
+
+  fps = (vs1 + vs2 + vd1 + vd2) * 0.25f;
+  update_display = true;
+  state = IDLE;
+}
+
+void arm()
+{
+  ts1_entry = 0, ts1_exit = 0, ts2_entry = 0, ts2_exit = 0;
+  sensor1_level = digitalRead(sensorPin1);
+  sensor2_level = digitalRead(sensorPin2);
+  old_sensor1_level = sensor1_level;
+  old_sensor2_level = sensor2_level;
+  state = ARMED;
+  last_display_blink = millis();
+  blink_state = true;
 }
